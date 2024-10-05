d7f5b1903cb257f6032d111a963013586f6a6b03
Ankit gujre <144484381+ankitgujre01@users.noreply.github.com>
2024-10-05 10:05:09 +0530

    Add files via upload

--- Employee.java
+++ Employee.java
@@ -1,30 +1,27 @@
-import javax.swing.*;
-
-class MainEmployee{
-    String name;
-    String addr;
-    String email;
-    double salary;
-
-    void setDetails(){
-        name = JOptionPane.showInputDialog("enter employee name");
-        addr = JOptionPane.showInputDialog("enter employee address");
-        email = JOptionPane.showInputDialog("enter employee email");
-        salary =Double.parseDouble(JOptionPane.showInputDialog("enter employee salary"));
+package com.employee;
+import com.customer.Customer;
+public class Employee
+{
+    int cust_id =0;
+    public int gen_customer_id(String cust_name)
+    {
+        if(cust_name.equals(""))
+        {
+            return 0;
+        }
+        else
+        {
+            cust_id++;
+        }
+        return cust_id;
     }
-    void getDetails(){
-        JOptionPane.showMessageDialog(null, "Employee Name" + " " + name);
-        JOptionPane.showMessageDialog(null, "Employee adress" + " " + addr);
-        JOptionPane.showMessageDialog(null, "Employee email" + " " + email);
-        JOptionPane.showMessageDialog(null, "Employee salary" + " " + salary);
-    }
-}
-public class Employee {
-    public static void main(String[] args) {
-
-        MainEmployee emp = new MainEmployee();
-        emp.setDetails();
-        emp.getDetails();
-
+    public static void main(String []str)
+    {
+        Employee manager = new Employee();
+        Customer amit = new Customer();
+        amit.setName("Amit Kumar");
+        int id= manager.gen_customer_id(amit.getName());
+        System.out.println("New Customer id"+"\t"+id);
+        System.out.println(amit.getName());
     }
 }
